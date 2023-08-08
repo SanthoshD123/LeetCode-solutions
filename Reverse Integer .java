8696203afdadcf4537ba959b5d6070017e7ce497
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-08-08 20:01:11 +0530

    Create Reverse Integer .java

---
+++ Reverse Integer .java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        
+        while (x != 0) {
+            int digit = x % 10;
+            
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; 
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; 
+            }
+            
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+        
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.reverse(1534236469);
+        System.out.println(result); // Output: 0
+    }
+}
