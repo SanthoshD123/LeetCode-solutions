2fca0bb390df2b2897fe75f986837dfe81e1899f
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-09-18 17:45:57 +0530

    Create 605. Can Place Flowers .java

---
+++ 605. Can Place Flowers .java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int i = 0;
+
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0) {
+                if (i == 0 || flowerbed[i - 1] == 0) {
+                    if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
+                        flowerbed[i] = 1;
+                        count++;
+                    }
+                }
+            }
+            i++;
+        }
+
+        return count >= n;
+    }
+}
